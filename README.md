dcf0d685863d8196bf738c257684ed4d0a74e4cf
Gustavo Silva <Gustavo Silva>
2021-05-06 14:53:31 -0300

    Sexta Version, se aumento .gitginore y Read me para poder subirlo a github

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pic of the Website
+![Captura 1](https://user-images.githubusercontent.com/66889974/117340916-d8687e00-ae77-11eb-9ee4-1aa4b9e8ff31.JPG)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const express = require('express'); //llamo modulo express
 const router = express.Router(); //llamo uno de los metdos de express que es router para creacion de multiples rutas
 const { v4: uuidv4 } = require('uuid'); //llamo a propiedad v4 (generacion aleatoria de mi uuid) denominada uuidv4 que viene de modulo uuid
 
+/* ESTO FUNCIONA DE FORMA ESTABLE PARA UNA SOLA PAGINA
 router.get('/', (req, res) => { //creo ruta hacia pagina principal
     //res.render('room.html'); //aca defino que es room
     res.redirect(`/${uuidv4()}`); //redireecciono mi pagina a ese uuid aleatorio para que sea unico mi room lo pongo entre`${}` ya que es un valor dinamco no esttico
@@ -11,5 +12,21 @@ router.get('/', (req, res) => { //creo ruta hacia pagina principal
 router.get('/:room', (req, res) => { //parametro dinamico que lo solicito luego
     res.render('room.html', { roomId: req.params.room }); //le solicito ir hacia mi archivo room.html mediante ese parametro generado llamado room que lo pido con req y lo denomino como roomId
 })
+
+module.exports = router; //exporto el router que se encarga de crear mis rutas
+*/
+
+//SEGUNDA FORMA CON PAGINA PRINCIPAL Y LA REDIRECCIONO HACIA ROOM
+router.get('/', (req,res) => { //creo ruta hacia pagina principal
+    res.render('index.html'); //pinto por pantalla el index
+});
+
+router.get('/room', (req, res) => { //creo ruta hacia room donde va a estar todo mi video chat
+    res.redirect(`/${uuidv4()}`); //redireecciono mi pagina a ese uuid aleatorio para que sea unico mi room lo pongo entre`${}` ya que es un valor dinamco no esttico
+});
+
+router.get('/:room', (req, res) => { //parametro dinamico que lo solicito luego
+    res.render('room.html', { roomId: req.params.room }); //le solicito ir hacia mi archivo room.html mediante ese parametro generado llamado room que lo pido con req y lo denomino como roomId
+});
 
 module.exports = router; //exporto el router que se encarga de crear mis rutas
